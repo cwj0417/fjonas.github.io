14c0d451e0500724e2e473cf7f674afa983f59b6
chen wen jun <731028571@qq.com>
2016-09-22 18:17:31 +0800

    update

--- source/_posts/es6-in-depth-rest-parameters-and-defaults.md
+++ source/_posts/es6-in-depth-rest-parameters-and-defaults.md
@@ -1,7 +1,15 @@
 ---
-title: es6 in depth rest parameters and defaults
+title: es6 in depth rest参数与默认参数
 date: 2016-09-10 17:25:30
 categories: 代码
 tags: [ES2015,es6-in-depth,翻译]
 ---
+今天,我们来讲一下es6让js语法更有表达性的两个特性: rest参数和默认参数.
+
+## rest参数
+
+这是一个普遍的需求, 当需要创建一个可以接受任何数量参数的函数, 叫做无限参数函数(variadic function). 
+
+---
+
 es in depth 系列 [目录](/2016/09/10/es6-in-depth-content/) [原文地址](https://hacks.mozilla.org/category/es6-in-depth/)
--- themes/maupassant/source/js/main.js
+++ themes/maupassant/source/js/main.js
@@ -1,8 +1,18 @@
 $(function() {
+	var config = {
+		cursor:['/images/ico/you.ico','/images/ico/riko.ico','/images/ico/chika.ico']
+	};
+	var changeCursor = function(url) {
+		$('html').css('cursor', 'url(' + url +'), auto');
+	};
 	$('a').each(function() {
 		var href = $(this).attr('href');
 		if(href && href.substr(0, 4) == 'http') {
 			$(this).attr('target', '_blank');
 		}	
-	})
-})
+	});
+	changeCursor(config.cursor[Math.floor(Math.random()*config.cursor.length)]);
+	setInterval(function() {
+		changeCursor(config.cursor[Math.floor(Math.random()*config.cursor.length)]);
+	}, 60000)
+});
