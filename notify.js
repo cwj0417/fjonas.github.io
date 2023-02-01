4741133566a5ac1f69032c13192926a7556efc3c
chenwj <731028571@qq.com>
2023-02-01 23:14:55 +0800

    2019summary

--- notify.js
+++ notify.js
@@ -1,62 +1,50 @@
 const request = require('axios');
-const sg = require('simple-git');
 const fs = require('fs-extra');
-''
+
 const appid = process.argv[2];
 const appsecret = process.argv[3];
+const filename = `source/_posts/${process.argv[4]}.md`;
 
-if (!appid || !appsecret) {
+if (!appid || !appsecret || !process.argv[4]) {
     console.log('apak not found');
     return;
 }
 
 const picmid = 'rM9vKgwYh7rfR-t1xSaBSMo1eWr6-MHBnkhBIxBBliLv3vP4Oq0jnYec0Hp4n1a7';
 
-const git = sg();
-console.log('fetch git log...');
-
-(async () => {
-    const res = await git.log({ 'maxCount': 1, '--stat': true, '--full-diff': false })
-    try {
-        console.log('fetched', res.latest.diff);
-        const content = fs.readFileSync(res.all[0].diff.files[0].file).toString();
-        const titleRegex = /---\ntitle:\s+(.*)\n/;
-        const dateRegex = /date:\s+(\d{4}-\d{2}-\d{2})/;
-        const breifRegex = /\n---\n((:?.|\n)*)<!--more-->\n/;
-        const title = titleRegex.exec(content)[1];
-        console.log({ title });
-        const breif = breifRegex.exec(content)[1];
-        console.log({ breif });
-        const date = dateRegex.exec(content)[1];
-        console.log({ date });
-        const url = `https://yo-cwj.com/${date.replace(/-/g, '/')}/${res.all[0].diff.files[0].file.replace(/.*\/([^/]*)\.md/, '$1')}`
-        console.log('wechat actions start');
+try {
+    const content = fs.readFileSync(filename).toString();
+    const titleRegex = /---\ntitle:\s+(.*)\n/;
+    const dateRegex = /date:\s+(\d{4}-\d{2}-\d{2})/;
+    const breifRegex = /\n---\n((:?.|\n)*)<!--more-->\n/;
+    const title = titleRegex.exec(content)[1];
+    const breif = breifRegex.exec(content)[1];
+    const date = dateRegex.exec(content)[1];
+    const url = `https://yo-cwj.com/${date.replace(/-/g, '/')}/${filename.replace(/.*\/([^/]*)\.md/, '$1')}`
+    request({
+        url: `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${appsecret}`
+    }).then(res => {
+        console.log(res.data);
+        const token = res.data.access_token;
         request({
-            url: `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${appsecret}`
-        }).then(res => {
-            console.log(res.data);
-            const token = res.data.access_token;
-            request({
-                url: `https://api.weixin.qq.com/cgi-bin/draft/add?access_token=${token}`,
-                method: 'POST',
-                data: {
-                    "articles": [
-                        {
-                            "title": title,
-                            "author": 'chenwj',
-                            "digest": breif.substring(0, 120),
-                            "content": `${breif.split(`\n`).map(i => `<p>${i}</p>`).join('')}
+            url: `https://api.weixin.qq.com/cgi-bin/draft/add?access_token=${token}`,
+            method: 'POST',
+            data: {
+                "articles": [
+                    {
+                        "title": title,
+                        "author": 'chenwj',
+                        "digest": breif.substring(0, 120),
+                        "content": `${breif.split(`\n`).map(i => `<p>${i}</p>`).join('')}
                             <br/>
                             <p>点击"阅读原文", 以获得更好的观看体验.</p>`,
-                            "content_source_url": url,
-                            "thumb_media_id": picmid,
-                            "need_open_comment": 1,
-                            "only_fans_can_comment": 1
-                        }
-                    ]
-                }
-            }).then(res => console.log(res.data.media_id))
-        })
-    } catch (e) { }
-
-})()
+                        "content_source_url": url,
+                        "thumb_media_id": picmid,
+                        "need_open_comment": 1,
+                        "only_fans_can_comment": 1
+                    }
+                ]
+            }
+        }).then(res => console.log(res.data.media_id))
+    })
+} catch (e) { }
