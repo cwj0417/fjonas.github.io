f27b6379e53b8cd4893a1057e363ba70d2cf2bbe
cwj0417 <chenwenjun@yo-cwj.com>
2026-05-28 02:44:29 +0800

    feat: update post.md and push.sh

--- push.sh
+++ push.sh
@@ -1,1 +1,1 @@
-git add . && git commit -m'update' && git push blog source:source
+git add . && git commit -m'update' && git push blog source:source && echo 'done'
--- scaffolds/post.md
+++ scaffolds/post.md
@@ -3,6 +3,7 @@ title: {{ title }}
 date: {{ date }}
 categories: 工作笔记 读后感
 tags:
+ai: false
 ---
 <!--more-->
 {% raw %}
